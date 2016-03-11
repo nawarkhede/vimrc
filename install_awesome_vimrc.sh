2b748f0765be0d2b2c9166f2cf43d7fe72caebab
Nishant Nawarkhede <nishant.nawarkhede@gmail.com>
2016-03-11 20:46:16 +0530

    added my personal changes

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -10,6 +10,17 @@ source ~/.vim_runtime/vimrcs/extended.vim
 try
 source ~/.vim_runtime/my_configs.vim
 catch
-endtry' > ~/.vimrc
+endtry
+
+" @nawarkhede vimrc custom settings
+"
+
+
+let g:syntastic_auto_loc_list = 0
+let g:syntastic_python_checkers=['flake8']
+   
+set cursorcolumn
+set cursorline
+colorscheme ir_black' > ~/.vimrc
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
